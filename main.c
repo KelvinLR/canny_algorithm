f58dead09c4ee906363adc3f922b55bd11153aee
Kelvin <kelvindelima710@gmail.com>
2025-10-27 17:20:02 -0300

    fix: adjusts at gaussian blur

--- main.c
+++ main.c
@@ -22,7 +22,6 @@ int img[89][89] = {0};
 int dir[89][89] = {0};
 int bordas[89][89] = {0};
 int largura, altura;
-//se declara-las dentro da função de ler pgm, o valor delas vai ser perdido depois que a função terminar de executar
 
 void ler_pgm(const char* filename) {
     FILE* f = fopen(filename, "r");
@@ -84,194 +83,138 @@ void salvar_pgm(const char* nome_arquivo, int matriz[89][89]) {
 void filtro_gaussiano() {
     int temp[89][89]; // buffer temporário
     float kernel[5][5] = {
-            {2/159.0f, 4/159.0f, 5/159.0f, 4/159.0f, 2/159.0f},
-            {4/159.0f, 9/159.0f, 12/159.0f, 9/159.0f, 4/159.0f},
-            {5/159.0f, 12/159.0f, 15/159.0f, 12/159.0f, 5/159.0f},
-            {4/159.0f, 9/159.0f, 12/159.0f, 9/159.0f, 4/159.0f},
-            {2/159.0f, 4/159.0f, 5/159.0f, 4/159.0f, 2/159.0f}
-        };
+        {2.0f/159.0f, 4.0f/159.0f, 5.0f/159.0f, 4.0f/159.0f, 2.0f/159.0f},
+        {4.0f/159.0f, 9.0f/159.0f, 12.0f/159.0f, 9.0f/159.0f, 4.0f/159.0f},
+        {5.0f/159.0f, 12.0f/159.0f, 15.0f/159.0f, 12.0f/159.0f, 5.0f/159.0f},
+        {4.0f/159.0f, 9.0f/159.0f, 12.0f/159.0f, 9.0f/159.0f, 4.0f/159.0f},
+        {2.0f/159.0f, 4.0f/159.0f, 5.0f/159.0f, 4.0f/159.0f, 2.0f/159.0f}
+    };
 
-        int k = 2; // kernel size // 5 // 2 from kernel_size/2
+    int k = 2; // kernel_size / 2
+    int i, j, m, n;
+    float sum;
+    int row_idx, col_idx;
 
-        int i, j, m, n;
-        float sum;
-        int row_idx, col_idx;
+    // Percorre cada pixel da imagem de saída
+    for(i = 0; i < altura; i++) {
+        for(j = 0; j < largura; j++) {
+            sum = 0.0f;
 
-        // Percorre cada pixel da imagem de saída
-        for(i = 0; i < altura; i++) {
-            for(j = 0; j < largura; j++) {
-                sum = 0.0f;
-
-                // Aplica o kernel na região da imagem considerando o padding com "modo-edge"
-                for(m = -k; m <= k; m++) {
-                    for(n = -k; n <= k; n++) {
-                        // Índices da imagem original, aplicando padding "modo-edge"
-                        row_idx = i + m;
-                        if(row_idx < 0)
-                            row_idx = 0;
-                        else if(row_idx >= altura)
-                            row_idx = altura - 1;
-
-                        col_idx = j + n;
-                        if(col_idx < 0)
-                            col_idx = 0;
-                        else if(col_idx >= largura)
-                            col_idx = largura - 1;
-
-                        sum += img[row_idx][col_idx] * kernel[m + k][n + k];
-                    }
+            // Aplica o kernel na região da imagem considerando o padding com "modo-edge"
+            for(m = -k; m <= k; m++) {
+                for(n = -k; n <= k; n++) {
+                    row_idx = i + m;
+                    if(row_idx < 0) row_idx = 0;
+                    else if(row_idx >= altura) row_idx = altura - 1;
+                    col_idx = j + n;
+                    if(col_idx < 0) col_idx = 0;
+                    else if(col_idx >= largura) col_idx = largura - 1;
+                    sum += img[row_idx][col_idx] * kernel[m + k][n + k];
                 }
-
-                // Clamp + conversão para int [0, 255]
-                if(sum < 0.0f) sum = 0.0f;
-                if(sum > 255.0f) sum = 255.0f;
-
-                temp[i][j] = (int)sum;
             }
-        }
 
-    salvar_pgm("gaussiano_teste.pgm", temp);
+            // Clamp para faixa [0, 255], SEM arredondamento ― igual Python int()
+            if(sum < 0.0f) sum = 0.0f;
+            if(sum > 255.0f) sum = 255.0f;
+            temp[i][j] = (int)sum; // truncamento, igual Python
+        }
+    }
+
+    salvar_pgm("gauss.pgm", temp); // Usa o buffer temporário igual ao seu modelo
 }
 
 void calc_gradiente() {
-    int direcao;
-    int temp_mag[89][89] = {0};
-    int sobelGx[3][3] = {{-1,0,1}, {-2,0,2}, {-1,0,1}};
-    int sobelGy[3][3] = {{-1,-2,-1}, {0,0,0}, {1,2,1}};
+    unsigned char temp_mag[89][89] = {0};
+    int sobelGx[3][3] = {{-1,0,1},{-2,0,2},{-1,0,1}};
+    int sobelGy[3][3] = {{-1,-2,-1},{0,0,0},{1,2,1}};
 
     for(int i = 1; i < 88; i++) {
         for(int j = 1; j < 88; j++) {
-            int soma_x = 0, soma_y = 0;
-            
-            for(int ki = -1; ki <= 1; ki++) {
+            float gx = 0, gy = 0;
+            for(int ki = -1; ki <= 1; ki++)
                 for(int kj = -1; kj <= 1; kj++) {
                     int pixel = img[i + ki][j + kj];
-                    soma_x += pixel * sobelGx[ki + 1][kj + 1];
-                    soma_y += pixel * sobelGy[ki + 1][kj + 1];
+                    gx += (float)pixel * sobelGx[ki + 1][kj + 1];
+                    gy += (float)pixel * sobelGy[ki + 1][kj + 1];
                 }
-            }
-            
-            temp_mag[i][j] = (int)sqrt(soma_x * soma_x + soma_y * soma_y);
+            float mag = sqrt(gx*gx + gy*gy);
+            if (mag > 255.0f) mag = 255.0f;
+            if (mag < 0.0f) mag = 0.0f;
+            temp_mag[i][j] = (unsigned char)mag; // truncamento igual a uint8
 
-            //direção do gradiente (em graus)
-            float angulo = atan2(soma_y, soma_x) * 180/PI;
-            if (angulo < 0) angulo += 180;
-
-            if (angulo<22.5 || angulo>=157.5)
-            {
-                direcao = 0;
-            }
-            else if (angulo<67.5)
-            {
-                direcao = 45;
-            }
+            float angulo = atan2(gy, gx) * 180.0f / PI;
+            if (angulo < 0) angulo += 180.0f;
+            if (angulo < 22.5 || angulo >= 157.5)
+                dir[i][j] = 0;
+            else if (angulo < 67.5)
+                dir[i][j] = 45;
             else if (angulo < 112.5)
-            {
-                direcao = 90;
-            }
-            else direcao = 135;
-
-            dir[i][j] = direcao;
-
-
+                dir[i][j] = 90;
+            else
+                dir[i][j] = 135;
         }
     }
-
-    for(int i = 0; i < 89; i++) {
-        for(int j = 0; j < 89; j++) {
+    for(int i = 0; i < 89; i++)
+        for(int j = 0; j < 89; j++)
             img[i][j] = temp_mag[i][j];
-        }
-    }
 
-    salvar_pgm("gradiente_teste.pgm",temp_mag);
 }
 
 void supr_nao_max() {
-    for (int i = 1; i < altura; i++){
-        for (int j = 1; j < largura; j++){
-            int mag = img[i][j];
+    for (int i = 1; i < altura-1; i++) {
+        for (int j = 1; j < largura-1; j++) {
+            unsigned char mag = img[i][j];
             int direcao = dir[i][j];
-            int n1, n2;
-
-            switch (direcao)
-            {
-            case 0:
-                n1 = img[i][j-1];
-                n2 = img[i][j+1];
-                break;
-            case 45:
-                n1 = img[i-1][j+1];
-                n2 = img[i+1][j-1];
-                break;
-            case 90:
-                n1 = img[i-1][j];
-                n2 = img[i+1][j];
-                break;
-            case 135:
-                n1 = img[i-1][j-1];
-                n2 = img[i+1][j+1];
-                break;
-            default:
-                n1 = n2 = 0;
+            unsigned char n1, n2;
+            switch (direcao) {
+                case 0:   n1 = img[i][j-1];   n2 = img[i][j+1];   break;
+                case 45:  n1 = img[i-1][j+1]; n2 = img[i+1][j-1]; break;
+                case 90:  n1 = img[i-1][j];   n2 = img[i+1][j];   break;
+                case 135: n1 = img[i-1][j-1]; n2 = img[i+1][j+1]; break;
+                default:  n1 = n2 = 0;
             }
-
-            if(mag >= n1 && mag >=  n2)
-            bordas[i][j] = mag > 255 ? 255 : mag;
-            else
-            bordas[i][j] = 0;
+            bordas[i][j] = (mag >= n1 && mag >= n2) ? mag : 0;
         }
     }
-    salvar_pgm("supressao_teste.pgm",bordas);
 }
 
 void limiarizacao_histerese(int limiar_baixo, int limiar_alto) {
     unsigned char forte = 255;
-    unsigned char fraco =  75;
+    unsigned char fraco = 75;
 
-    for (int i = 0; i < altura; i++){
-        for (int j = 0; j < largura; j++){
-
+    for (int i = 0; i < altura; i++)
+        for (int j = 0; j < largura; j++)
             if (bordas[i][j] >= limiar_alto)
                 bordas[i][j] = forte;
             else if (bordas[i][j] >= limiar_baixo)
                 bordas[i][j] = fraco;
             else
                 bordas[i][j] = 0;
-            
-        }
-    }
 
     int mudou = 1;
-    while (mudou){
+    while (mudou) {
         mudou = 0;
-        for (int i = 1; i < altura -1; i++){
-            for (int j = 1; j < largura-1; j++){
-                if (bordas[i][j] == fraco){
-                    int tem_forte = 0;
-                    for (int di = -1; di <= 1; di++){
-                        for (int dj = -1; dj <= 1; dj++){
-                            if(bordas[i + di][j + dj] == forte) {
-                                tem_forte = 1;
-                                break;
+        for (int i = 1; i < altura-1; i++) {
+            for (int j = 1; j < largura-1; j++) {
+                if (bordas[i][j] == fraco) {
+                    for (int di = -1; di <= 1; di++)
+                        for (int dj = -1; dj <= 1; dj++) {
+                            if (bordas[i+di][j+dj] == forte) {
+                                bordas[i][j] = forte;
+                                mudou = 1;
+                                goto next;
                             }
                         }
-                        if(tem_forte) break;
-                    }
-                    if(tem_forte) {
-                        bordas[i][j] = forte;
-                        mudou = 1;
-                    }
+                    next: ;
                 }
             }
         }
     }
-    
-    for (int i = 0; i < altura; i++){
-        for (int j = 0; j < largura; j++){
-            if(bordas[i][j] == fraco)
+
+    for (int i = 0; i < altura; i++)
+        for (int j = 0; j < largura; j++)
+            if (bordas[i][j] == fraco)
                 bordas[i][j] = 0;
-        }
-    }
 }
 
 float compare_images(int img1[89][89], int img2[89][89]) {
